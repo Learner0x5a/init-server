ec80a1ba0161dbcf0d3937b6a1fef7e989b02a16
Learner0x5a <2739805116@qq.com>
2021-01-08 15:03:07 +0800

    Create init-ubuntu-20.04.1.sh

---
+++ init-ubuntu-20.04.1.sh
@@ -0,0 +1,15 @@
+# software sources
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+sudo echo sources/ubuntu-20.04.1 > /etc/apt/sources.list
+sudo apt update
+sudo apt install vim git zsh -y
+
+# zsh
+sh zsh/zsh_install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+cp zsh/zshrc ~/.zshrc
+source ~/.zshrc
+
+# vim
+cp vim/vimrc ~/.vimtc
