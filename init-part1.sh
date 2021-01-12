a3eb05192f2578839e614f125923bcadcc6b2c96
Learner0x5a <2739805116@qq.com>
2021-01-12 14:50:39 +0800

    Update init-part1.sh

--- init-part1.sh
+++ init-part1.sh
@@ -1,7 +1,7 @@
 
 # software sources
-cp /etc/apt/sources.list /etc/apt/sources.list.backup
-cp sources/ubuntu-18.04  /etc/apt/sources.list
+# cp /etc/apt/sources.list /etc/apt/sources.list.backup
+# cp sources/ubuntu-18.04  /etc/apt/sources.list
 apt update
 apt install vim git zsh -y
 
